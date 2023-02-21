5b0083a4186f30c8cf2aaa1666dc5ae0c35a53db
evin <evinlerouge@icloud.com>
2023-02-21 12:33:27 -0700

    x and o functionality working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 //imports
+import React, { useState } from 'react';
 import './App.css';
 import { Board } from './components/Board.js'
 //=================================================
 //=================================================
 function App() {
-  const board = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
+  // hooks
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xPlaying, setXPlaying] = useState(true);
+  // const board = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
+  //=============================================================
+  //=============================================================
+  // functions
+  const handleBoxClick = (boxIdx) => {
+    const updatedBoard = board.map((value, idx) => {
+      if (idx === boxIdx) {
+        return xPlaying === true ? 'X' : 'O';
+      } else {
+        return value;
+      }
+    })
+    setBoard(updatedBoard);
+    setXPlaying(!xPlaying);
+  }
+
+
   return (
     <div className="App">
-      <Board board={board} onClick={null} />
+      <Board board={board} onClick={handleBoxClick} />
     </div>
 
   );
